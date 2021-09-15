0051ce93574ab589f16f2e362924d8b33ca6fe48
Tomas <tomas.milkintas1@gmail.com>
2021-09-15 14:00:32 +0100

    adjusting main body

--- src/Main/Header.js
+++ src/Main/Header.js
@@ -5,14 +5,15 @@ const Header = () => {
     return (
         <div className="headerSection">
             <div className="split">
-                <img
+                {/* <img
                     src="https://renify.s3.amazonaws.com/system/uploads/images/1712/Stainless_steel_worktops_-_Holte_Kitchens.jpg"
                     alt="work top"
                     className="image"
-                />
+                /> */}
                 <div className="imgDescr">
                     <div className="text">
                         <h2 className="textHeading">Bespoke furniture</h2>
+
                         {/* Loft Conversions
                         Timber Framed Buildings
                         Doors
@@ -23,14 +24,15 @@ const Header = () => {
                         Handyman Service
                         Extensions
                         Renewing, repairing, replacement*/}
-                        <p>
+                        {/* <p>
                             Kitchen units assembled to the highest quality. Lorem ipsum dolor sit
                             amet consectetur adipisicing elit. Dolorem sequi quis eum blanditiis
                             alias, ad laudantium dignissimos aliquam? Laudantium rerum ab ex
                             tempore. Commodi ab veniam voluptatibus reiciendis incidunt, in dolore
                             odit officia ratione magnam cum quidem voluptate eos est?
-                        </p>
+                        </p> */}
                     </div>
+                    <div className="blobHorizontal"></div>
                 </div>
             </div>
         </div>
